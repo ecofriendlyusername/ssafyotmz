07bfc2c11d7c2a62ced1535076377702a97a555e
preferchoi <74041004+preferchoi@users.noreply.github.com>
2023-03-08 17:08:05 +0900

    feat:add_vuex,vue_router

--- frontend/src/components/KakaoLogin.vue
+++ frontend/src/components/KakaoLogin.vue
@@ -1,27 +1,39 @@
 <template>
     <section>
         <p>1234</p>
-        <button type="submit"></button>
-        
+        <button type="submit" v-on:click="loginWithKakao">login</button>
     </section>
 </template>
 
-<script src="https://t1.kakaocdn.net/kakao_js_sdk/2.1.0/kakao.min.js"
-    integrity="sha384-dpu02ieKC6NUeKFoGMOKz6102CLEWi9+5RQjWSV0ikYSFFd8M3Wp2reIcquJOemx" crossorigin="anonymous"></script>
+<script src="https://t1.kakaocdn.net/kakao_js_sdk/2.1.0/kakao.min.js" integrity="sha384-dpu02ieKC6NUeKFoGMOKz6102CLEWi9+5RQjWSV0ikYSFFd8M3Wp2reIcquJOemx" crossorigin="anonymous"></script>
+
 <script>
-    // SDK를 초기화 합니다. 사용할 앱의 JavaScript 키를 설정해야 합니다.
-
-
+	// $.cookie('kakaoUserSecedeAuth', null, {path:"/"});
+	// $.cookie('kakaoUserEditAuth', null, {path:"/"});
 </script>
+
 <script>
 export default {
-    name:'KakaoLoginVue',
+name:'KakaoLoginVue',
     methods:{
-        log: function () {
-            Kakao.init("d351596851492e4d0a8828d5497305e8");
+        // 카카오 로그인
+        loginWithKakao: function () {
+            // $.cookie('kakao_prev_uri', '/users/login', {path:"/"});	// 카카오 호출 전 현재 페이지 저장
 
-            // SDK 초기화 여부를 판단합니다.
-            console.log(Kakao.isInitialized());
+            Kakao.Auth.authorize({
+                redirectUri : 'http://localhost:8080'
+            });
+
+        Kakao.API.request({
+            url: '/v2/user/me',
+            data: {
+                property_keys: ['kakao_account.email', 'kakao_account.gender'],
+                },
+        }).then(function(response) {
+            console.log(response);
+        }).catch(function(error) {
+            console.log(error);
+            });
         }
     }
 }
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import store from './store'
+import VueCookies from "vue-cookies";
 
 Vue.config.productionTip = false
+Vue.use(VueCookies);
 
 new Vue({
-  render: h => h(App),
+  router,
+  store,
+  render: h => h(App)
 }).$mount('#app')
