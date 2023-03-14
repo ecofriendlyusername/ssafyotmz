bcb8cce225471ee428156100610c5903a14104da
Bellroute <kjkun7631@gmail.com>
2023-03-14 14:26:56 +0900

    Feat: ㅠBaseEntity ì추가

--- backend/otmz-api/src/main/java/com/patandmat/otmz/domain/member/entity/Member.java
+++ backend/otmz-api/src/main/java/com/patandmat/otmz/domain/member/entity/Member.java
@@ -1,5 +1,6 @@
 package com.patandmat.otmz.domain.member.entity;
 
+import com.patandmat.otmz.domain.common.BaseEntity;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -9,7 +10,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Member {
+public class Member extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
