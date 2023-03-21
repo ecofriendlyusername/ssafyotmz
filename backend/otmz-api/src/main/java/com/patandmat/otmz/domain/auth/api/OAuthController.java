edff35d19c0fa24020d679741c5c7734256daa83
Bellroute <kjkun7631@gmail.com>
2023-03-21 11:33:51 +0900

    refactor: 카카오 로그인 응답 dto  객체 변경 (Map -> TokenResponse)
    Former-commit-id: b0501b0b3e809478da76a6080bc7f560067e1306

--- backend/otmz-api/src/main/java/com/patandmat/otmz/domain/auth/api/OAuthController.java
+++ backend/otmz-api/src/main/java/com/patandmat/otmz/domain/auth/api/OAuthController.java
@@ -1,5 +1,6 @@
 package com.patandmat.otmz.domain.auth.api;
 
+import com.patandmat.otmz.domain.auth.api.model.TokenResponse;
 import com.patandmat.otmz.domain.auth.application.JwtService;
 import com.patandmat.otmz.domain.auth.application.OAuthService;
 import com.patandmat.otmz.domain.member.entity.Member;
@@ -20,8 +21,7 @@ public class OAuthController {
 
     @ResponseBody
     @GetMapping("/kakao")
-    public ResponseEntity<Map<String,String>> kakaoCallback(@RequestParam String code) {
-        Map<String, String> map = new HashMap<>();
+    public ResponseEntity<TokenResponse> kakaoCallback(@RequestParam String code) {
         Map<String , Object> result = oauthService.getKakaoAccessToken(code);
 
         Member member = oauthService.getKakaoUser(result.get("access_Token").toString());
@@ -31,9 +31,7 @@ public class OAuthController {
 
         String accessToken = jwtService.createAccessToken("id", member.getId());
         String refreshToken = jwtService.createRefreshToken("id", member.getId());
-        map.put("access_token", accessToken);
-        map.put("refresh_token", refreshToken);
 
-        return new ResponseEntity<>(map, HttpStatus.OK);
+        return new ResponseEntity<>(new TokenResponse(accessToken, refreshToken), HttpStatus.OK);
     }
 }
