c202a6a1bcefb53484290937e1b089c0a3c96363
preferchoi <74041004+preferchoi@users.noreply.github.com>
2023-03-21 09:08:41 +0900

    feat:set_init_route
    Former-commit-id: a8f10379476ec10bbb3a2c3e7b9317598e397cac

---
+++ frontend/src/views/Find/ResultView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <router-link to=''>스타일 저장하기</router-link> |
+    <router-link to=''>스타일 공유하기</router-link> |
+    <router-link to='/Recom'>다른 스타일 보기</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'FindResultView',
+}
+</script>
+
+<style>
+
+</style>
