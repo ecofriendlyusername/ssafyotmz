86796d97ccb9920c0658fc65d90bdabe8d024ef4
Bellroute <kjkun7631@gmail.com>
2023-03-21 13:41:58 +0900

    refactor: security config에서 web config 분리
    Former-commit-id: ac0109b192a97d46e7aa14c6238b67dc0e96d854

--- backend/otmz-api/src/main/java/com/patandmat/otmz/global/config/SecurityConfig.java
+++ backend/otmz-api/src/main/java/com/patandmat/otmz/global/config/SecurityConfig.java
@@ -1,10 +1,8 @@
 package com.patandmat.otmz.global.config;
 
-import com.patandmat.otmz.global.interceptor.TokenCheckInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -14,36 +12,11 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.Arrays;
 
 @EnableWebSecurity
 @RequiredArgsConstructor
 @Configuration
-public class SecurityConfig implements WebMvcConfigurer {
-
-    private final TokenCheckInterceptor tokenCheckInterceptor;
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods(HttpMethod.GET.name(), HttpMethod.POST.name(), HttpMethod.PUT.name(),
-                        HttpMethod.DELETE.name(), HttpMethod.HEAD.name(), HttpMethod.OPTIONS.name(),
-                        HttpMethod.PATCH.name())
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH", "HEAD")
-                .maxAge(1800);
-    }
-
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(tokenCheckInterceptor)
-                .addPathPatterns("/**")
-                .excludePathPatterns(Arrays.asList("/oauth/kakao/**", "/swagger-ui/**", "/v3/api-docs/**"));
-    }
+public class SecurityConfig {
 
     @Bean
     public InMemoryUserDetailsManager userDetailsService(PasswordEncoder passwordEncoder) {
