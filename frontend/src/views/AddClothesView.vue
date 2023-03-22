e903421774717717f66e1208a0f9d31f155acb91
gayoung.yang.0 <gayoung.yang.0@gmail.com>
2023-03-22 17:35:34 +0900

    mid save
    Former-commit-id: 79475ef2d4e816572e30ec8ba8c01f79ff2422cd

--- frontend/src/views/AddClothesView.vue
+++ frontend/src/views/AddClothesView.vue
@@ -1,22 +1,52 @@
 <template>
+<div>
   <div>
     옷 추가하기
   </div>
   <hr>
-  <form action="#">
-    <label for="">옷 이미지를 추가하세요</label><input type="file" name="" id="" >
+  <form id="itemForm" @submit.prevent="createItemWith" action="#">
+    <label for="imagefile">옷 이미지를 추가하세요</label><input type="file" id="imagefile" name="imagefile">
     <br>
-    <label for="">옷 이름을 입력하세요</label><input type="text">
+    <label for="">옷 이름을 입력하세요</label><input type="text" id="name" name="name">
     <br>
+    <br>
+    <input type="file"
+       id="avatar" name="avatar"
+       accept="image/png, image/jpeg">
+       <br>
     <input type="submit" value="추가하기">
   </form>
   <hr>
-  <router-link to='/'>메인페이지</router-link>
+  <!-- <router-link to='/'>메인페이지</router-link> -->
+</div>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
-    name:'AddClothesView',
+  name:'AddClothesView',
+  data () {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    createItemWith() {
+      var itemForm = new FormData(document.getElementById('itemForm'))
+      var imageFile = itemForm.get("imagefile")
+      var name = itemForm.get("name")
+      this.createItem(imageFile,name)
+    },
+    createItem(imagefile,item) {
+      axios.post(process.env.VUE_APP_ITEM,{imagefile,item},{headers: {
+        "Content-Type": "application/json;charset=utf-8",
+        "Authorization": process.env.local.TOKEN,
+      }})
+      .then(response => {
+        console.log(response)
+      })
+    }
+  }
 }
 </script>
 
