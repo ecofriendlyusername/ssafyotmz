aa3b061a2406ed0388490d3fa7a2327fcebc82ff
yangtotang <yyj5126@kangwon.ac.kr>
2023-03-22 13:21:04 +0900

    feat: look 등록

---
+++ backend/otmz-api/src/main/java/com/patandmat/otmz/domain/look/api/model/LookController.java
@@ -0,0 +1,50 @@
+package com.patandmat.otmz.domain.look.api.model;
+
+import com.patandmat.otmz.domain.item.exception.NoSuchMemberException;
+import com.patandmat.otmz.domain.look.application.LookService;
+import com.patandmat.otmz.domain.member.entity.Member;
+import com.patandmat.otmz.global.auth.CustomUserDetails;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.management.AttributeNotFoundException;
+
+import java.io.IOException;
+import java.util.NoSuchElementException;
+
+@RestController
+@RequiredArgsConstructor
+public class LookController {
+    private final String SUCCESS = "SUCCESS";
+    private final String FAIL = "FAIL";
+    private final LookService lookService;
+
+    @PostMapping("/look")
+    public ResponseEntity<?> saveLook(@RequestPart("imagefile") MultipartFile file, @RequestBody String style, Authentication authentication) throws IOException {
+
+        CustomUserDetails userDetails = (CustomUserDetails) authentication.getDetails();
+        Member member = userDetails.getMember();
+
+
+        try {
+            lookService.saveLook(file,style,member.getId());
+        } catch (NoSuchElementException e) {
+            return new ResponseEntity<>("User Does Not Exist", HttpStatus.BAD_REQUEST);
+        } catch (AttributeNotFoundException e) {
+            return new ResponseEntity<>("Check Attributes of The Item", HttpStatus.BAD_REQUEST);
+        } catch (NoSuchMemberException e) {
+            return new ResponseEntity<>("User Doesn't Exist", HttpStatus.BAD_REQUEST);
+        }
+
+
+        return ResponseEntity.ok().build();
+    }
+
+}
