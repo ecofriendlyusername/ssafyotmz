3231cd318e2130d89b690ed4fee931978398cf4f
Bellroute <kjkun7631@gmail.com>
2023-03-22 16:35:52 +0900

    refactor: 코사인 유사도 입력값 변경 (list -> map)

--- backend/otmz-api/src/main/java/com/patandmat/otmz/global/utils/SimilarityMeasurer.java
+++ backend/otmz-api/src/main/java/com/patandmat/otmz/global/utils/SimilarityMeasurer.java
@@ -1,21 +1,25 @@
 package com.patandmat.otmz.global.utils;
 
-import java.util.List;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 public class SimilarityMeasurer {
 
-    public static Double getCosineSimilarity(final List<Double> vector1, final List<Double> vector2) {
+    public static Double getCosineSimilarity(final Map<String, Double> vector1, final Map<String, Double> vector2) {
         if (vector1 == null || vector2 == null) {
             throw new IllegalArgumentException("Vectors must not be null");
         }
 
-        final double dotProduct = calculateDotProduct(vector1, vector2);
+        Set<String> intersection = getIntersection(vector1, vector2);
+        final double dotProduct = calculateDotProduct(vector1, vector2, intersection);
+
         double sum1 = 0.0d;
-        for (final Double value : vector1) {
+        for (final Double value : vector1.values()) {
             sum1 += Math.pow(value, 2);
         }
         double sum2 = 0.0d;
-        for (final Double value : vector2) {
+        for (final Double value : vector2.values()) {
             sum2 += Math.pow(value, 2);
         }
 
@@ -30,10 +34,17 @@ public class SimilarityMeasurer {
         return (double) Math.round(cosineSimilarity * 100) / 100;
     }
 
-    private static double calculateDotProduct(final List<Double> vector1, final List<Double> vector2) {
+    private static Set<String> getIntersection(Map<String, Double> vector1, Map<String, Double> vector2) {
+        Set<String> set = new HashSet<>(vector1.keySet());
+        set.addAll(vector2.keySet());
+
+        return set;
+    }
+
+    private static double calculateDotProduct(final Map<String, Double> vector1, final Map<String, Double> vector2, final Set<String> intersection) {
         long dotProduct = 0;
-        for (int i = 0; i < vector1.size(); i++) {
-            dotProduct += vector1.get(i) * vector2.get(i);
+        for (String key : intersection) {
+            dotProduct += vector1.get(key) * vector2.get(key);
         }
 
         return dotProduct;
