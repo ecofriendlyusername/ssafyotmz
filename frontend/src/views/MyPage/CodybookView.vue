411eb4af8d66eac172daa5ed6ffc38110ac1616d
preferchoi <74041004+preferchoi@users.noreply.github.com>
2023-03-22 10:38:15 +0900

    tag_set:MyPage/*

--- frontend/src/views/MyPage/CodybookView.vue
+++ frontend/src/views/MyPage/CodybookView.vue
@@ -2,7 +2,25 @@
   <div>
     내 코디북 페이지
   </div>
+  <!-- 스타일 별 코디북 -->
+  <ul>
+    <li>
+      <img src="@/assets/img/logo.png" alt="">
+      <p>코디북 이름</p>
+    </li>
 
+    <li>
+      <img src="@/assets/img/logo.png" alt="">
+      <p>코디북 이름</p>
+    </li>
+
+    <li>
+      <img src="@/assets/img/logo.png" alt="">
+      <p>코디북 이름</p>
+    </li>
+  </ul>
+
+  <router-link to='/Codybook'>코디 등록</router-link>
   <router-link to='/MyPage'>마이페이지</router-link>
 </template>
 
