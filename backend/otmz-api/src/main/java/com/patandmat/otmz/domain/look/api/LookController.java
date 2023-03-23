7950bcc36a9a4c5152015a9afa4148bbd0fe57ff
Bellroute <kjkun7631@gmail.com>
2023-03-23 14:07:56 +0900

    feature: 스타일 추천 알고리즘 및 api 구현
    Former-commit-id: da78209ab11281832af1b1d6f33da7ea0432473e

--- backend/otmz-api/src/main/java/com/patandmat/otmz/domain/look/api/LookController.java
+++ backend/otmz-api/src/main/java/com/patandmat/otmz/domain/look/api/LookController.java
@@ -1,6 +1,8 @@
 package com.patandmat.otmz.domain.look.api;
 
 import com.patandmat.otmz.domain.item.exception.NoSuchMemberException;
+import com.patandmat.otmz.domain.look.api.model.RecommendedLookResponse;
+import com.patandmat.otmz.domain.look.application.LookRecommendService;
 import com.patandmat.otmz.domain.look.application.LookService;
 import com.patandmat.otmz.domain.member.entity.Member;
 import com.patandmat.otmz.global.auth.CustomUserDetails;
@@ -8,23 +10,20 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.management.AttributeNotFoundException;
-
 import java.io.IOException;
+import java.util.List;
 import java.util.NoSuchElementException;
 
 @RestController
 @RequiredArgsConstructor
 public class LookController {
-    private final String SUCCESS = "SUCCESS";
-    private final String FAIL = "FAIL";
     private final LookService lookService;
+
+    private final LookRecommendService lookRecommendService;
 
     @PostMapping("/look")
     public ResponseEntity<?> saveLook(@RequestPart("imagefile") MultipartFile file, @RequestBody String style, Authentication authentication) throws IOException {
@@ -33,7 +32,7 @@ public class LookController {
         Member member = userDetails.getMember();
 
         try {
-            lookService.saveLook(file,style,member.getId());
+            lookService.saveLook(file, style, member.getId());
         } catch (NoSuchElementException e) {
             return new ResponseEntity<>("User Does Not Exist", HttpStatus.BAD_REQUEST);
         } catch (AttributeNotFoundException e) {
@@ -45,4 +44,16 @@ public class LookController {
         return ResponseEntity.ok().build();
     }
 
+    @GetMapping("/look/recommended")
+    public ResponseEntity<List<RecommendedLookResponse>> recommendLooks(Authentication authentication,
+                                                                        @RequestParam(required = false, defaultValue = "3") Integer size,
+                                                                        @RequestParam(required = false, defaultValue = "false") boolean reversed) {
+
+        CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
+        Member member = userDetails.getMember();
+
+        List<RecommendedLookResponse> looks = lookRecommendService.getRecommendedLooks(member, size, reversed);
+
+        return ResponseEntity.ok(looks);
+    }
 }
