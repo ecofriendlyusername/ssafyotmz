48fcba5d916c83dfc60e60689ac87a100fb978a6
preferchoi <74041004+preferchoi@users.noreply.github.com>
2023-03-23 09:48:01 +0900

    feat:mainPageBranchSettings
    Former-commit-id: d51df82d4da99953376c912589e0c325ef2f6490

--- frontend/src/views/MainView.vue
+++ frontend/src/views/MainView.vue
@@ -45,7 +45,7 @@ export default {
     let auth = null;
     if (code) {
       console.log(code)
-      axios.get(process.env.VUE_APP_KAKAO_CERTIFIED_API_URL + '?code=' + code + '&redirect_uri=http://localhost:3000')
+      axios.get(process.env.VUE_APP_KAKAO_CERTIFIED_API_URL + '?code=' + code)
         .then(response => {
           this.$store.commit('setAuth', response.data); // auth 값을 스토어에 저장
         })
