3fa345135c52a7412e844cca6b21e413e1eeed9f
Bellroute <kjkun7631@gmail.com>
2023-03-28 15:42:39 +0900

    chore: 코드 라인 정리
    Former-commit-id: a9d10aca2aab2a93770116996af64f3afe6f61af

--- backend/otmz-api/src/main/java/com/patandmat/otmz/domain/member/api/MemberController.java
+++ backend/otmz-api/src/main/java/com/patandmat/otmz/domain/member/api/MemberController.java
@@ -17,7 +17,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
