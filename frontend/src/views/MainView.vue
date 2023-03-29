b957e73434716338c549ef8a48a7e9d453f44034
preferchoi <74041004+preferchoi@users.noreply.github.com>
2023-03-29 16:34:24 +0900

    feature/fe/로그인 메인화면 swiper  구현
    Former-commit-id: cf6d004da9ae9d1d81f6ff016ed236a4274b6979

--- frontend/src/views/MainView.vue
+++ frontend/src/views/MainView.vue
@@ -1,5 +1,56 @@
 <template>
-  <div v-if="Auth.memberId">{{ Auth }}</div>
+  <!-- 로그인 시 메인화면 -->
+  <div v-if="Auth.memberId" style="font-family: 'NanumSquareNeo-Variable';">
+    <swiper
+      :modules="modules"
+      :space-between="20"
+      :loop="true"
+      :pagination="{clickable: true}"  
+      :autoplay="{
+        delay: 7000,
+        disableOnInteraction: false,
+        pauseOnMouseEnter: true
+      }"
+    >
+
+    <swiper-slide v-for="text in swiperTextBase" :key="text.description">
+      
+      <img :src="text.img" alt="image" style="width:100%; height: 280px; opacity:1 ; ">
+      <div style="position: absolute; top: 0px; background-color:black; opacity:0.5; height:280px; width:100%">
+      </div>
+
+      <div style="position:absolute; top:200px;">
+        <p style="color: white; display: flex; font-weight: bold; font-size: 18px; padding: 5px 7px;">{{ text.description }}</p>
+        <p style="font-size: 13px; color: white; display: flex; padding-left: 7px; margin-top: -16px;">
+          {{ text.author }}
+        </p>
+      </div>
+
+      <div style="position:absolute; top:10px;font-size: 14.5px; color:white; margin-left: 78%;">
+          → 구매하기
+      </div>
+
+    </swiper-slide>
+    </swiper>
+
+
+
+    <div>
+      <div id="LoginDiv1">
+        {{ Auth['nickname'] }} 님의 취향이 담긴 코디
+      </div>
+      <div id="LoginDiv2">
+        # 러블리
+      </div>
+
+    </div>
+    
+    <!-- {{ Auth }} -->
+  </div>
+
+
+
+  <!-- 비로그인 시 메인화면 -->
   <div v-else style="background-color:black" class="main">
     <div id="start-div" style="height:820px; background-color:#FBFBFD">
       <img data-aos="zoom-in" data-aos-duration="2000" src="@/assets/img/main1.png" style="width:100.3%">
@@ -57,9 +108,34 @@ import 'aos/dist/aos.css'; // You can also use <link> for styles
 AOS.init();
 
 
+import {Swiper, SwiperSlide} from 'swiper/vue'
+import {Autoplay, Pagination} from 'swiper'
+import 'swiper/css'
+import 'swiper/css/pagination'
+import {ref} from 'vue'
+
 
 export default {
   name: 'MainView',
+  components: {Swiper, SwiperSlide},
+  setup() {
+    const swiperTextBase = ref([
+      {author:'러브앤드팝',
+      description: '[UNISEX] 미니멀 오버핏셔츠',
+      img: 'https://d20s70j9gw443i.cloudfront.net/t_GOODS_DETAIL/https://imgb.a-bly.com/data/goods/20210225_1614235708677593m.jpg'
+      },
+      {author:'아더먼데이',
+      description: '미니쉘 기본 오버핏셔츠',
+      img: 'https://d20s70j9gw443i.cloudfront.net/t_GOODS_DETAIL/https://imgb.a-bly.com/data/goods/20230126_1674699972522098m.jpg'
+      },
+      {author:'바이영',
+      description: '봄가을 소매트임 베이직 자켓',
+      img: 'https://d20s70j9gw443i.cloudfront.net/t_GOODS_DETAIL/https://imgb.a-bly.com/data/goods/20200916_1600266495690951m.JPG'
+      },
+    ]);
+    return {modules:[Pagination, Autoplay], swiperTextBase}
+
+  },
   data() {
     return {
       EndPoint: false,
@@ -122,6 +198,7 @@ export default {
 </script>
 
 <style>
+
 .fade-in-box {
   display: inline-block;
   margin: 10px 0px 60px 0px;
@@ -148,4 +225,22 @@ export default {
 .fade-enter-to {
   opacity: 1;
   transform: translateY(0px);
-}</style>
+}
+
+#LoginDiv1 {
+  display: flex;
+  padding: 10px 15px;
+  font-size: 18.5px;
+  font-weight: bold;
+}
+
+#LoginDiv2 {
+  display: flex;
+  margin-top: -3px;
+  padding: 0px 15px;
+  font-size: 16px;
+  font-weight: bold;
+}
+
+
+</style>
