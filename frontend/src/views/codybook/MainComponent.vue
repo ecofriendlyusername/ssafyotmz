6c71628aec4f501c7e21faa42637082c3e6f3736
Bellroute <kjkun7631@gmail.com>
2023-03-30 01:39:18 +0900

    feature: openvidu 연결 기능 일부 구현 - 실시간 통신 입장 - 통신에 참여중인 사용자 목록 - 최초 입장 시 코디 보드 상태 동기화
    Former-commit-id: 954ac0587c5606e674a8f4d24fdfeac0510d99b5

---
+++ frontend/src/views/codybook/MainComponent.vue
@@ -0,0 +1,528 @@
+<template>
+<div>
+  <div id="main-container" class="container">
+    <div id="join" v-if="!session">
+      <div id="join-dialog" class="jumbotron vertical-center">
+        <h1>Join a video session</h1>
+        <div class="form-group">
+          <p>
+            <label>Participant</label>
+            <input v-model="myUserName" class="form-control" type="text" required />
+          </p>
+          <p>
+            <label>Session</label>
+            <input v-model="mySessionId" class="form-control" type="text" required />
+          </p>
+          <p class="text-center">
+            <button class="btn btn-lg btn-success" @click="joinSession()">
+              Join!
+            </button>
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <div id="session" v-if="session">
+      <div id="session-header">
+        <h1 id="session-title">{{ mySessionId }}</h1>
+          <div>
+            라이브 코디북 만들기 페이지
+          </div>
+        <input class="btn btn-large btn-danger" type="button" id="buttonLeaveSession" @click="leaveSession"
+          value="Leave session" />
+      </div>
+      <hr>
+      <div>
+        <p @click="getItems('outer')">아우터</p>
+        <p @click="getItems('upper')">상의</p>
+        <p @click="getItems('lower')">하의</p>
+        <p @click="getItems('dress')">원피스</p>
+        <p @click="getItems('etc')">기타</p>
+      </div>
+      <div v-for="item in items" @click="choice(item)" :key="item.id">
+        <div>{{item}}</div>
+        <img :src='`${item.src}`' style="width:100px;hegiht:100px"/>
+      </div>
+      <hr>
+      <div id="video-container">
+        <user-video :stream-manager="publisher" :isSpeaking="this.publisher.isSpeaking && audioEnabled" />
+        <user-video v-for="sub in subscribers" :key="sub.stream.connection.connectionId" :stream-manager="sub" :isSpeaking="sub.isSpeaking"/>
+      </div>
+      <div v-show="audioEnabled" @click="micOnOff" style="width:50px;height:50px">
+        <img src="https://collusic.com/assets/track/unselected/vocal.svg" >
+      </div>
+      <div v-show="!audioEnabled" @click="micOnOff" style="width:50px;height:50px">
+        <img  src="https://collusic.com/assets/track/unselected/crap.svg" >
+      </div>
+      <hr>
+      <div>
+        현재 코디북에 등록된 옷
+        <div>{{list}}</div>
+        <div v-if="dragItemId" @click="removeItem">삭제</div>
+        <div :style="{margin: '10px', backgroundColor: backgroundColor}">
+          <v-stage
+            ref="stage"
+            :config="configKonva"
+            @dragstart="handleDragstart"
+            @dragend="handleDragend"
+            @mousedown="handleMouseDown"
+            @touchstart="handleMouseDown"
+          >
+            <v-layer ref="layer"
+            :config="configKonva">
+              <v-image v-for="item in list" :key="item.id"
+                :config="{
+                  name: item.name,
+                  image: item.image,
+                  x: item.x,
+                  y: item.y,
+                  width: item.width,
+                  height: item.height,
+                  rotation: item.rotation,
+                  id: item.id,
+                  numPoints: 5,
+                  innerRadius: 30,
+                  outerRadius: 50,
+                  draggable: item.dragable,
+                  shadowColor: 'black',
+                  shadowBlur: 10,
+                  shadowOffsetX: dragItemId === item.id ? 10 : 3,
+                  shadowOffsetY: dragItemId === item.id ? 10 : 3,
+                  shadowOpacity: 0.2,
+                }"
+                @transformend="handleTransformEnd"
+              ></v-image>
+              <v-transformer ref="transformer"/>
+            </v-layer>
+          </v-stage>
+        </div>
+        <div @click="clear">clear</div>
+        <div class="box">
+          <span class="editor-btn icon" title="Color Picker">
+            <input title="Color Picker" type="color" id="color" v-model="backgroundColor">
+          </span>
+        </div>
+      </div>
+      <hr>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+import axios from "axios";
+import { OpenVidu } from "openvidu-browser";
+import UserVideo from "./UserVideo";
+
+const width = 400;
+const height = 400;
+
+axios.defaults.headers.post["Content-Type"] = "application/json";
+
+const APPLICATION_SERVER_URL = process.env.NODE_ENV === 'production' ? '' : 'http://localhost:8000/';
+
+export default {
+  name: 'MainComponent',
+
+  components: {
+    UserVideo,
+  },
+
+  data() {
+    return {
+      // OpenVidu objects
+      OV: undefined,
+      session: undefined,
+      mainStreamManager: undefined,
+      publisher: undefined,
+      subscribers: [],
+      audioEnabled: true,
+      hello: undefined,
+
+      // Join form
+      mySessionId: "SessionA",
+      myUserName: "Participant" + Math.floor(Math.random() * 100),
+
+      items: [],
+      count: 0,
+      list: [],
+      dragItemId: null,
+      configKonva: {
+        width: width,
+        height: height
+      },
+      backgroundColor: '#FFDAB9'
+    };
+  },
+
+  mounted() {
+    this.backgroundColor = '#' +Math.floor(Math.random()*16777215).toString(16);
+  },
+
+  methods: {
+    removeItem() {
+      if (this.dragItemId === null) {
+        return;
+      }
+        const index = this.list.findIndex(i => i.name === this.dragItemId);
+        this.list.splice(index, 1);
+        this.dragItemId = null;
+        this.updateTransformer();
+    },
+    handleTransformEnd(e) {
+      const item = this.list.find(i => i.name === this.dragItemId);
+      item.x = e.target.x();
+      item.y = e.target.y();
+      item.rotation = e.target.rotation();
+      item.scaleX = e.target.scaleX();
+      item.scaleY = e.target.scaleY();
+      item.width = e.target.width();
+      item.height = e.target.height();
+    },
+    handleMouseDown(e) {
+      console.log('click')
+      if (e.target === e.target.getStage()) {
+        this.dragItemId = null;
+        this.updateTransformer();
+        return;
+      }
+
+      const clickedOnTransformer = e.target.getParent().className === 'Transformer';
+      if (clickedOnTransformer) {
+        return;
+      }
+
+      const name = e.target.name();
+      const item = this.list.find(i => i.name === name);
+      if (item) {
+        this.dragItemId = name;
+        this.updateTransformer();
+        const index = this.list.indexOf(item);
+        this.list.splice(index, 1);
+        this.list.push(item);
+      } else {
+        this.dragItemId = null;
+      }
+    },
+    clear() {
+      this.list = []
+      this.dragItemId = null;
+      this.updateTransformer();
+    },
+    handleDragstart(e) {
+      // save drag element:
+      this.dragItemId = e.target.id();
+      // move current element to the top:
+      const item = this.list.find(i => i.id === this.dragItemId);
+      const index = this.list.indexOf(item);
+      this.list.splice(index, 1);
+      this.list.push(item);
+
+      this.updateTransformer();
+    },
+    handleDragend(e) {
+      const item = this.list.find(i => i.name === this.dragItemId);
+
+      console.log(this.list.find(i => i.name === this.dragItemId));
+
+      item.x = e.target.x();
+      item.y = e.target.y();
+      item.rotation = e.target.rotation();
+
+    },
+    updateTransformer() {
+      const transformerNode = this.$refs.transformer.getNode();
+      const stage = transformerNode.getStage();
+      const { dragItemId } = this;
+
+      const selectedNode = stage.findOne('.' + dragItemId);
+      if (selectedNode === transformerNode.node()) {
+        return;
+      }
+
+      if (selectedNode) {
+        transformerNode.nodes([selectedNode]);
+      } else {
+        transformerNode.nodes([]);
+      }
+    },
+    choice(item) {
+      const img = new Image();
+      img.src = process.env.VUE_APP_DEFAULT_API_URL + '/api/v1/images/' + item.imageId;
+
+      const targetIndex = this.list.findIndex(x => x.name === String(item.id));
+      this.dragItemId = null;
+      this.updateTransformer();
+
+      if (targetIndex !== -1) {
+        const target = this.list[targetIndex];
+        this.list.splice(targetIndex, 1);
+        this.list.push(target);
+        return;
+      }
+    
+      this.list.push({
+        name: String(item.id),
+        image: img,
+        id: String(item.id),
+        x: item.x ? item.x : 10,
+        y: item.y ? item.y : 10,
+        rotation: 0,
+        width: item.width ? item.width : 200,
+        height: item.height ? item.height : 200,
+        scale: 1,
+        dragable: true
+      });
+    },
+    getItems(category) {
+      axios.get(process.env.VUE_APP_DEFAULT_API_URL + '/api/v1/items/' + category +'?page=0&size=10', { // outer, upper, lower, dress, etc
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': this.$store.state.Auth['accessToken']
+        }
+      })
+      .then((response) => {
+        // 파일 저장하는 api 리턴값으로 파일 경로 달라고 해야 함
+        // this.image = response.data
+        console.log(response.data)
+        this.items = response.data.content;
+        this.items.forEach(item => item.src = process.env.VUE_APP_DEFAULT_API_URL + '/api/v1/images/' + item.imageId);
+      })
+      .catch(error =>{
+        console.log(error)
+      });
+    },
+    micOnOff() {
+        this.audioEnabled = !this.audioEnabled;
+        this.publisher.publishAudio(this.audioEnabled);
+    },
+    alterSpeaker(id, isSpeaking) {
+      this.session.signal({
+        data: JSON.stringify({id: id, isSpeaking: isSpeaking}),
+        to: this.subscribers,
+        type: 'alterSpeaker'
+      }).then(() => {
+        console.log('Message successfully sent');
+      })
+      .catch(error => {
+          console.error(error);
+      });
+    },
+    alertMoveItem(x, y) {
+      this.session.signal({
+        data: JSON.stringify({x: x, y: y}),
+        to: this.subscribers,
+        type: 'moveItem'
+      }).then(() => {
+        console.log('Message successfully sent');
+      })
+      .catch(error => {
+          console.error(error);
+      });
+    },
+    alertSetUpBoard() {
+      console.log('items', this.list)
+      const data = [];
+      this.list.forEach(i => {
+        const item = Object.assign({}, i);
+        item.image = i.image.src;
+        data.push(item)
+      });
+      console.log(data);
+      this.session.signal({
+        data: JSON.stringify(data),
+        to: this.subscribers.filter(subscriber => subscriber !== this.publisher),
+        type: 'setUpBoard'
+      }).then(() => {
+        console.log('Message successfully sent');
+      })
+      .catch(error => {
+          console.error(error);
+      });
+    },
+    joinSession() {
+      // --- 1) Get an OpenVidu object ---
+      this.OV = new OpenVidu();
+
+      // --- 2) Init a session ---
+      this.session = this.OV.initSession();
+
+      // --- 3) Specify the actions when events take place in the session ---
+
+      // On every new Stream received...
+      this.session.on("streamCreated", ({ stream }) => {
+        const subscriber = this.session.subscribe(stream);
+        subscriber.isSpeaking = false;
+        this.subscribers.push(subscriber);
+        if (this.mySessionId === this.myUserName) {
+          this.alertSetUpBoard();
+        }
+      });
+
+      // On every Stream destroyed...
+      this.session.on("streamDestroyed", ({ stream }) => {
+        const index = this.subscribers.indexOf(stream.streamManager, 0);
+        if (index >= 0) {
+          this.subscribers.splice(index, 1);
+        }
+      });
+
+      // On every asynchronous exception...
+      this.session.on("exception", ({ exception }) => {
+        console.warn(exception);
+      });
+
+      // --- 4) Connect to the session with a valid user token ---
+
+      // Get a token from the OpenVidu deployment
+      this.getToken(this.mySessionId).then((token) => {
+
+        // First param is the token. Second param can be retrieved by every user on event
+        // 'streamCreated' (property Stream.connection.data), and will be appended to DOM as the user's nickname
+        this.session.connect(token, { clientData: this.myUserName })
+          .then(() => {
+
+            // --- 5) Get your own camera stream with the desired properties ---
+
+            // Init a publisher passing undefined as targetElement (we don't want OpenVidu to insert a video
+            // element: we will manage it on our own) and with the desired properties
+            let publisher = this.OV.initPublisher(undefined, {
+              audioSource: undefined, // The source of audio. If undefined default microphone
+              videoSource: undefined, // The source of video. If undefined default webcam
+              publishAudio: true, // Whether you want to start publishing with your audio unmuted or not
+              publishVideo: false, // Whether you want to start publishing with your video enabled or not
+              resolution: "640x480", // The resolution of your video
+              frameRate: 30, // The frame rate of your video
+              insertMode: "APPEND", // How the video is inserted in the target element 'video-container'
+              mirror: false, // Whether to mirror your local video or not
+            });
+
+            // Set the main video in the page to display our webcam and store our Publisher
+            this.mainStreamManager = publisher;
+            this.publisher = publisher;
+            this.publisher.isSpeaking = false;
+            const { list } = this;
+
+            // --- 6) Publish your stream ---
+
+            this.session.publish(this.publisher);
+            this.session.on('publisherStartSpeaking', (event) => {
+                console.log('User ' + event.connection.connectionId + ' start speaking');
+                this.publisher.isSpeaking = true;
+                this.alterSpeaker(event.connection.connectionId, true);
+            });
+
+            this.session.on('publisherStopSpeaking', (event) => {
+                console.log('User ' + event.connection.connectionId + ' stop speaking');
+                this.publisher.isSpeaking = false;
+                this.alterSpeaker(event.connection.connectionId, false);
+            });
+
+            this.session.on('signal:setUpBoard', (event) => {
+              console.log('setUpBoard');
+              const data = JSON.parse(event.data);
+              console.log(data)
+              data.forEach(x => {
+                if (this.list.findIndex(i => i.name === x.name) === -1) {
+                  const item = x;
+                  const img = new Image();
+                  img.src = x.image;
+                  item.image = img;
+                  list.push(item)
+                }
+              });
+            })
+
+            this.session.on('signal:moveItem', (event) => {
+              console.log('moveItem');
+              const data = JSON.parse(event.data);
+              const clothes = document.getElementById('clothes1');
+              clothes.style.left = data.x;
+              clothes.style.top = data.y;
+            });
+
+            this.session.on('signal:alterSpeaker', (event) => {
+              console.log('alterSpeaker');
+              const data = JSON.parse(event.data)
+              console.log(data)
+              console.log(this.subscribers)
+              if (this.subscribers.length === 0) {
+                return;
+              }
+              let speaker = this.subscribers.find(sub => {
+                return sub.stream.connection.connectionId === data.id});
+              console.log(speaker)
+              if (speaker !== undefined) {
+                speaker.isSpeaking = data.isSpeaking;
+              }
+            });
+          })
+          .catch((error) => {
+            console.log("There was an error connecting to the session:", error.code, error.message);
+          });
+      });
+
+      window.addEventListener("beforeunload", this.leaveSession);
+    },
+
+    leaveSession() {
+      // --- 7) Leave the session by calling 'disconnect' method over the Session object ---
+      if (this.session) this.session.disconnect();
+
+      // Empty all properties...
+      this.session = undefined;
+      this.mainStreamManager = undefined;
+      this.publisher = undefined;
+      this.subscribers = [];
+      this.audioEnabled = true;
+      this.OV = undefined;
+
+      // Remove beforeunload listener
+      window.removeEventListener("beforeunload", this.leaveSession);
+    },
+
+    /**
+     * --------------------------------------------
+     * GETTING A TOKEN FROM YOUR APPLICATION SERVER
+     * --------------------------------------------
+     * The methods below request the creation of a Session and a Token to
+     * your application server. This keeps your OpenVidu deployment secure.
+     *
+     * In this sample code, there is no user control at all. Anybody could
+     * access your application server endpoints! In a real production
+     * environment, your application server must identify the user to allow
+     * access to the endpoints.
+     *
+     * Visit https://docs.openvidu.io/en/stable/application-server to learn
+     * more about the integration of OpenVidu in your application server.
+     */
+    async getToken(mySessionId) {
+      const sessionId = await this.createSession(mySessionId);
+      return await this.createToken(sessionId);
+    },
+
+    async createSession(sessionId) {
+      const response = await axios.post(APPLICATION_SERVER_URL + 'api/sessions', { customSessionId: sessionId }, {
+        headers: { 'Content-Type': 'application/json', },
+      });
+      return response.data; // The sessionId
+    },
+
+    async createToken(sessionId) {
+      const response = await axios.post(APPLICATION_SERVER_URL + 'api/sessions/' + sessionId + '/connections', {}, {
+        headers: { 'Content-Type': 'application/json', },
+      });
+      return response.data; // The token
+    },
+  },
+}
+</script>
+
+<style scoped>
+.mouse-img{
+    display: flex;
+    width: 200px;
+    height: 200px;
+    position: absolute;
+  }
+</style>
---
+++ frontend/src/views/codybook/UserVideo.vue
@@ -0,0 +1,71 @@
+<template>
+<div class="member" v-if="streamManager">
+	<div id="profile" :class="{ 'active': isSpeaking }">
+		<img src="https://k.kakaocdn.net/dn/CezCL/btrKatXtJFO/8X2UHivEyLVjnx9wZyHM9K/img_110x110.jpg">
+	</div>
+	<ov-video :stream-manager="streamManager"/>
+	<p>{{ clientData }}</p>
+	<p>{{ streamManager.stream.connection.connectionId}}</p>
+</div>
+</template>
+
+<script>
+import OvVideo from './OvVideo';
+
+export default {
+	name: 'UserVideo',
+
+	components: {
+		OvVideo,
+	},
+
+	props: {
+		streamManager: Object,
+		isSpeaking: Boolean,
+	},
+
+	computed: {
+		clientData () {
+			const { clientData } = this.getConnectionData();
+			return clientData;
+		},
+	},
+
+	methods: {
+		getConnectionData () {
+			const { connection } = this.streamManager.stream;
+			return JSON.parse(connection.data);
+		},
+	},
+};
+</script>
+
+<style>
+#profile {
+	width: 60px;
+	height: 60px;
+	border-radius: 50%;
+	border: 1px solid #e1e2e3;
+	display: -ms-flexbox;
+	display: flex;
+	-ms-flex-pack: center;
+	justify-content: center;
+	-ms-flex-align: center;
+	align-items: center;
+	overflow: hidden;
+}
+
+#profile > img {
+	width: 55px;
+  height: 55px;
+  border-radius: 50%;
+  background-position: 50%;
+  background-size: cover;
+  background-repeat: no-repeat;
+}
+
+.member {
+	text-align: left;
+	margin: 0 auto;
+}
+</style>
