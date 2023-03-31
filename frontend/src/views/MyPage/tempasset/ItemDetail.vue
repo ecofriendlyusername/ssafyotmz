ce26e180800c3e4ac05ba42db3535e7d240102aa
gayoungyang <gayoung.yang.0@gmail.com>
2023-03-31 23:40:19 +0900

    chore: minor fix and a little css

--- frontend/src/views/MyPage/tempasset/ItemDetail.vue
+++ frontend/src/views/MyPage/tempasset/ItemDetail.vue
@@ -1,16 +1,15 @@
 <template>
     <div>
         <div>
-          내 옷
+          <div>{{ this.selected.name }}</div>
         </div>
-        <hr>    
         <div id="item">
             <img :src="env+selected.imageId" width="100"/>
-            <div>{{ this.selected.name }}</div>
-            <div>{{ this.selected.comment }}</div>
         </div>
-        <button @click="deleteItem()" @touchstart="deleteItem">삭제</button>
-        <router-link to='/MyPage'>마이페이지</router-link>
+      <div class="selectionBox">
+        <div class="selection" @mousedown="deleteItem()">삭제</div>
+        <div class="selection" @mousedown="$emit('close')">Close</div>
+      </div>
     </div>
     </template>
       
@@ -22,9 +21,6 @@
     data () {
         return {
           env: process.env.VUE_APP_IMG,
-          image: "",
-          name: "",
-          category: "",
           Auth: this.$store.state.Auth
         }
     },
@@ -47,11 +43,4 @@
     </script>
       
     <style>
-    .row {
-      display: flex;
-    }
-    .column {
-      flex: 33.33%;
-      padding: 5px;
-    }
     </style>
