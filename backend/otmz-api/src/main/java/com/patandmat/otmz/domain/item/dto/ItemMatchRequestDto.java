cb63cc898e2bdb74f65aae0320399f62c63702d4
Bellroute <kjkun7631@gmail.com>
2023-04-03 15:17:01 +0900

    feature: 코디 보드 저장 기능 및 조회 화면 수정
    Former-commit-id: 9d90c4c209f96ea61eb0dfafeed7f6c161bce543

--- backend/otmz-api/src/main/java/com/patandmat/otmz/domain/item/dto/ItemMatchRequestDto.java
+++ backend/otmz-api/src/main/java/com/patandmat/otmz/domain/item/dto/ItemMatchRequestDto.java
@@ -1,10 +1,12 @@
 package com.patandmat.otmz.domain.item.dto;
 
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
+@AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ItemMatchRequestDto {
     private String name;
--- frontend/src/views/MyPage/ClosetView.vue
+++ frontend/src/views/MyPage/ClosetView.vue
@@ -24,7 +24,6 @@ export default {
   },
   methods: {
     changeCategory(category) {
-      this.selec
       this.curCategory = category;
     }
   },
