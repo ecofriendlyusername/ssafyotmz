d847c84a128cecc5d4acc99f6666466776c693db
preferchoi <74041004+preferchoi@users.noreply.github.com>
2023-04-03 11:18:41 +0900

    feature/fe/header 패딩 조절
    Former-commit-id: 0cd70b373d4e020e7d63fcadc7bbe5699622a154

--- frontend/src/components/molecules/header.vue
+++ frontend/src/components/molecules/header.vue
@@ -50,7 +50,7 @@ header {
   align-items: center;
   justify-content: space-between;
   height: 60px;
-  padding: 0 2px;
+  padding: 0 30px;
   background-color: rgb(255, 255, 255);
   border-bottom: rgb(220, 220, 220) solid 0.1px;
 }
