5188f357f95542f370eb83e94b8d51e1d3e0d8ce
Bellroute <kjkun7631@gmail.com>
2023-04-04 22:34:22 +0900

    feature: 초기 스타일 조사에 필요한 룩 이미지 목록 조회 api 구현
    Former-commit-id: 144aec6eb43d6e48f73a517045792c9fd098f364

--- backend/otmz-api/src/main/java/com/patandmat/otmz/domain/look/repository/LookRepository.java
+++ backend/otmz-api/src/main/java/com/patandmat/otmz/domain/look/repository/LookRepository.java
@@ -1,6 +1,5 @@
 package com.patandmat.otmz.domain.look.repository;
 
-import com.patandmat.otmz.domain.look.api.model.LookResponse;
 import com.patandmat.otmz.domain.look.api.model.StyleByCountResponse;
 import com.patandmat.otmz.domain.look.entity.Look;
 import com.patandmat.otmz.domain.look.entity.Style;
@@ -26,4 +25,7 @@ public interface LookRepository extends JpaRepository<Look, Long> {
     Page<Look> findAllByStyleOrderByCreatedAtDesc(Style style, Pageable pageable);
 
     Page<Look> findAllByMemberId(Long memberId, Pageable pageable);
+
+    @Query(nativeQuery = true, value = "SELECT * FROM Look l where style = :style LIMIT :limit")
+    List<Look> findByStyle(@Param("style") Style style, @Param("limit") int limit);
 }
