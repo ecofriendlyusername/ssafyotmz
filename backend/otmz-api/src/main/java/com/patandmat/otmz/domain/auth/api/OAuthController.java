1f759a8ba17fe9a938f88c30e23004a9cd3c2cf6
gayoungyang <gayoung.yang.0@gmail.com>
2023-04-04 23:06:09 +0900

    test items are added when a user joins
    Former-commit-id: fa16ffcd7b84c2a4fedbaa582509de338be006c2

--- backend/otmz-api/src/main/java/com/patandmat/otmz/domain/auth/api/OAuthController.java
+++ backend/otmz-api/src/main/java/com/patandmat/otmz/domain/auth/api/OAuthController.java
@@ -14,6 +14,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/oauth")
