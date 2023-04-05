7c00488cc173196206b709b9dd36dc780c6a1bb4
Bellroute <kjkun7631@gmail.com>
2023-04-05 11:28:53 +0900

    fix: í크롤링 ì데이터 업로드 및 조회 API 수정
    Former-commit-id: 3a91e780d5b92002c212b62d347bd40b1f0b2c08

--- backend/otmz-api/src/main/java/com/patandmat/otmz/domain/item/application/CrawledItemServiceImpl.java
+++ backend/otmz-api/src/main/java/com/patandmat/otmz/domain/item/application/CrawledItemServiceImpl.java
@@ -8,12 +8,14 @@ import com.patandmat.otmz.domain.member.entity.Member;
 import com.patandmat.otmz.global.utils.SimilarityMeasurer;
 import com.patandmat.otmz.global.utils.VectorParser;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class CrawledItemServiceImpl implements CrawledItemService {
@@ -37,7 +39,19 @@ public class CrawledItemServiceImpl implements CrawledItemService {
         List<ItemSimilarity> itemSimilarities = items.parallelStream()
                 .map((item -> new Object[]{item, VectorParser.parseToMap(item.getStyleVector(), VectorParser.STYLE_KEY, VectorParser.STYLE_VALUE)}))
                 .map(data -> new ItemSimilarity((CrawledItem) data[0], SimilarityMeasurer.getCosineSimilarity(memberStyleStats, (Map<String, Double>) data[1])))
-                .toList().stream().sorted().toList();
+                .toList().stream().sorted((t1, t2) -> {
+                    if (t1.getSimilarity() > t2.getSimilarity()) {
+                        return -1;
+                    } else if (t1.getSimilarity() < t2.getSimilarity()) {
+                        return 1;
+                    }
+                    return 0;
+                }).toList();
+
+        for (int i = 0; i < 10; i++) {
+            ItemSimilarity similarity = itemSimilarities.get(i);
+            log.info("similarity result: {}, {}, {}", similarity.getItem().getId(), similarity.getItem().getName(), similarity.getSimilarity());
+        }
 
         return itemSimilarities
                 .subList(0, 10)
