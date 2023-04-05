ac9b28818f8031ffdcdafcb77531b6f30714cdb5
Bellroute <kjkun7631@gmail.com>
2023-04-06 00:27:07 +0900

    feature: redis 설정 추가
    Former-commit-id: 66ff4d9475e4fb6e1be337da0ed87fcf02af4ff2

---
+++ backend/otmz-api/src/main/java/com/patandmat/otmz/global/utils/CacheKey.java
@@ -0,0 +1,7 @@
+package com.patandmat.otmz.global.utils;
+
+public class CacheKey {
+    public static final int DEFAULT_EXPIRE_SEC = 600; // 10 min
+    public static final int POST_EXPIRE_SEC = 3600; // 1 hour
+    public static final String POST = "post";
+}
